29584c72cd7d450c1a8e51ea08f33345004a6f1d
Sadik Moazzem <sadik-moazzem@outlook.com>
2025-01-29 16:30:16 +0000

    Updated netlify config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  compress: true,
+  optimizeFonts: true,
+  images: {
+    unoptimized: true,
+    domains: ['www.mylocalmasjid.com'],
+  },
 };
 
 export default nextConfig;
